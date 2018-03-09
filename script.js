5d44ff793ee85e4ffe0047b9e6a444610f4d7d1c
Cyndi Norwood <cyndinorwood@me.com>
2018-03-09 11:47:43 -0800

    fixed social media row

--- script.js
+++ script.js
@@ -3,13 +3,26 @@
 //document.getElementByClassName("alert").addEventListener("click", function(){
 //    document.getElementByClassName("bell").alert = "Hello World";
 //});
+var src = document.getElementById('searchUser');
+var msg = document.getElementById('messageUser');
+var buttonA = document.querySelector('.btn-a');
 
 
-function ValidationEvent(){
-	if ((document.searchUser.search.value=="") || (document.userForm.fname.value=="")){
+
+  buttonA.addEventListener('click', function() {
+    if (src.value == "" || msg.value == "") {
+      src.value = "Please select a user and type in message!";
+    } else {
+      document.getElementById("searchUser").value = "Submitted!";
+    }
+});
+
+/*function ValidationEvent(){
+	if ((document.searchUser.search.value=="") || (document.searchUser.fname.value=="")){
 		alert("Search for user or message for user is empty, \n please fill in required fields!")
 		return false
 	}
 	else
-		return true
+		fname.value = "Sumitted";
 }
+*/
