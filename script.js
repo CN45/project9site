da6e82c190ca9d6b6e7e130b16895de32b8196d9
Cyndi Norwood <cyndinorwood@me.com>
2018-03-11 19:25:31 -0700

    fixed javascript in message user area

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
 
 
-//document.getElementByClassName("alert").addEventListener("click", function(){
-//    document.getElementByClassName("bell").alert = "Hello World";
-//});
-var src = document.getElementById('searchUser');
-var msg = document.getElementById('messageUser');
-var buttonA = document.querySelector('.btn-a');
+
+
+var src = document.querySelector('.src-user');
+var msg = document.querySelector('.message-user');
+var buttonA = document.querySelector('.btn-bar');
 
 
 
-  buttonA.addEventListener('click', function() {
-    if (src.value == "" || msg.value == "") {
-      src.value = "Please select a user and type in message!";
-    } else {
-      document.getElementById("searchUser").value = "Submitted!";
-    }
-});
-
-/*function ValidationEvent(){
-	if ((document.searchUser.search.value=="") || (document.searchUser.fname.value=="")){
-		alert("Search for user or message for user is empty, \n please fill in required fields!")
-		return false
-	}
-	else
-		fname.value = "Sumitted";
+buttonA.addEventListener('click', function() {
+  if (src.value.length > 0 || msg.value.length > 0) {
+    src.value = "Submitted";
+  } else {
+    src.value = "Please enter a name";
 }
-*/
+});
